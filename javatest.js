d4c34e46a1f55e83abf996d6ef2f92b0f8bc7644
Maria Narvaez <narvaezmaria25@gmail.com>
2016-06-30 01:11:50 -0400

    update HTML and JS

---
+++ javatest.js
@@ -0,0 +1,34 @@
+// this is a test
+
+console.log("Hi Welcome");
+
+alert("Hi Welcome");
+
+function logToHTML() {
+   document.getElementById("title").innerHTML = "Welcome";
+}
+
+var x = 100;
+var o = 30;
+
+console.log(x - o);
+console.log(x + 10);
+console.log(x + o);
+
+var letters = [ "P", "Q", "A"];
+
+console.log(letters[0]);
+console.log(letters[2]);
+console.log(letters[3]);
+
+
+var word = 	"my";
+var	secondWord = "favorite";
+var	thirdWord = "color";
+var	fourthWord = "is";
+var	firstColor = "black";
+
+console.log(word + " " + secondWord + " " + thirdWord +  " " + fourthWord + " " +  firstColor);
+
+var thisiIsTrue = true;
+console.log(thisiIsTrue);
